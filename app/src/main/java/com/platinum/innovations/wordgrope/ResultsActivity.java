8e5916e185954dfa518f6a9ff468a224832233a1
Platinum <platinumemirate@gmail.com>
2020-06-09 19:24:05 +0100

    Added Firebase

--- app/src/main/java/com/platinum/innovations/wordgrope/ResultsActivity.java
+++ app/src/main/java/com/platinum/innovations/wordgrope/ResultsActivity.java
@@ -6,7 +6,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
